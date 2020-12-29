f541851edbdace33a2a51e56424a21c8d9ce1efc
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-12-29 12:42:40 -0500

    created footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './pages/Home';
 
@@ -11,6 +12,7 @@ function App() {
           <Route exact path='/' component={Home} />
         </Switch>
       </main>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import Logo from '../Logo/Logo';
+import Nav from '../Nav/Nav';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <Logo />
+      <Nav />
+    </footer>
+  );
+};
+
+export default Footer;
